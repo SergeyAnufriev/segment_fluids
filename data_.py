ed6e4e706ba3e3c9963f60d8840917f599f4503e
Sergey Anufriev <anufriiev.sergii@gmail.com>
2021-07-28 11:23:34 +0300

    add caviation and angle of attack to data_loader

--- data_.py
+++ data_.py
@@ -17,22 +17,34 @@ def cav_number(path):
     return cav
 
 
+def angle_(path):
+    '''Input : file name
+      Output : angle of attack in degress'''
+    file = path.split('\\')[-1]
+    props = file.split('_')
+
+    return  np.arctan(float(props[-4])/ float(props[-5]))*180/np.pi
+
+
+
 class Dataset_(Dataset):
-    def __init__(self, dir_,device,u_inf=2.):
+    def __init__(self, dir_,device,ro=1000,u_inf=2.):
         self.device   = device
         self.files_   = glob.glob(dir_)
         self.u_inf    = u_inf
+        self.ro       = ro
+
     def __len__(self):
         return len(self.files_)
 
-    @staticmethod
-    def pressure_norm(x):
+
+    def pressure_norm(self,x):
         '''(Ian's density norm)
         Input: pressure matrix
           return: normilised pressure'''
 
         '''P/(ro_*|v|^2)'''
-        x    = x/(1000*2**2)
+        x    = x/(self.ro*self.u_inf**2)
         mean = np.mean(x)
 
         return x - mean
@@ -50,9 +62,13 @@ class Dataset_(Dataset):
         values_[3]     = self.pressure_norm(values_[3])
 
         '''Get cavitation number'''
-        cav            = torch.full((1,128,128),cav_number(path_),device=self.device,dtype=torch.float32)
+        cav_n          = cav_number(path_)
+        cav            = torch.full((1,128,128),cav_n,device=self.device,dtype=torch.float32)
         flip_mask      = 1 - values_[2,:,:]
         cav            = cav*torch.tensor(flip_mask,device=self.device,dtype=torch.float32).view(1,128,128)
+
+        '''Angle of attack'''
+        alpa           = angle_(path_)
 
         '''Create model inputs'''
         Ux_Uy_mask     = torch.tensor(values_[:3,:,:],dtype=torch.float32,device=self.device)
@@ -61,7 +77,7 @@ class Dataset_(Dataset):
         '''create model outputs'''
         targets_       = torch.tensor(values_[3:,:,:],dtype=torch.float32,device=self.device)
 
-        return inputs_, targets_
+        return inputs_, targets_, cav_n,alpa
 
 
 
