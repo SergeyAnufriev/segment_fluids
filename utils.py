02491ffec3be7283c193c9da5169fc8f81e170f3
Sergey Anufriev <anufriiev.sergii@gmail.com>
2021-08-07 07:36:00 +0300

    fix relative error

--- utils.py
+++ utils.py
@@ -11,27 +11,25 @@ def L1_(output_,target_):
     return diff_.mean(dim=[0,2,3])
 
 
-def relative_error(output_,target_,mask_,alpha=0.1,eps=1e-6):
-
+def relative_error(output_, target_, mask_, alpha=0.1, eps=1e-6):
     '''Input:  output_, target_ with shape [1 * C * HIGHT * WIDTH] and mask [1 * HIGHT *WIDTH]
        Output: relative error with shape 1 * C '''
 
     '''Number of channels vary depending on if void fraction is calculated or not'''
-    n_channels    = target_.shape[1]
-    diff_         = abs(target_-output_)
+    n_channels = target_.shape[1]
+    diff_ = abs(target_ - output_)
 
+    mask_ = torch.cat([mask_.unsqueeze(1)] * n_channels, dim=1)
     '''mask pixels inside the airfoil'''
-    masked_diff_  = torch.masked_select(diff_,~(mask_.bool()))
-    masked_target = torch.masked_select(abs(target_),~(mask_.bool()))+eps
+    masked_diff_ = torch.masked_select(diff_, ~(mask_.bool()))
+    masked_target = torch.masked_select(abs(target_), ~(mask_.bool())) + eps
 
-    error         = masked_diff_/masked_target
-    l             = len(error)
-    error         = error.view(n_channels,int(l/n_channels))
-    error_indic_  = error>alpha
+    error = masked_diff_ / masked_target
+    l = len(error)
+    error = error.view(n_channels, int(l / n_channels))
+    error_indic_ = error > alpha
 
-    n_pixels_outside = 128**2-mask_.sum()
-
-    return error_indic_.sum(dim=1)/n_pixels_outside
+    return error_indic_.sum(dim=1) / (l / n_channels)
 
 
 def model_test(model,test_data_loader,n_var):
